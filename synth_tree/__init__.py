ac69978f0c7ebfad706d1387e7fc308d4535fd2c
ZhKuriabov <52123463+ZhKuriabov@users.noreply.github.com>
2025-05-25 17:01:43 -0400

    Update __init__.py

---
+++ synth_tree/__init__.py
@@ -0,0 +1,5 @@
+from .core.tree import SynthTree
+from .co_supervision.clustering import ClusteringManager
+from .co_supervision.local_models import LocalModelFitter
+
+__all__ = ["SynthTree", "ClusteringManager", "LocalModelFitter"]
